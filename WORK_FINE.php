6beb7f4c6f28685fdea17f554b4e099d2dce5947
Mvr <tolearntoshare@users.noreply.github.com>
2017-10-07 17:38:27 +0300

    the last working multi lang page enjoy! ;d

--- WORK_FINE.php
+++ WORK_FINE.php
@@ -8,8 +8,16 @@ if (!function_exists("gettext")) {
     //echo "<br>";
 }
 
-if (isset($_GET['lang'])) {
-    $language = $_GET['lang'];
+if (isset($_GET['lang']) ) {
+    if( $_GET['lang']=='ar_JO'){
+        echo "arabic";
+        $language = 'ar_JO'; 
+        include 'bodystart.arabic.template.php';
+    }
+    else{
+        $language = $_GET['lang']; 
+    }
+   
 } else {
     $language = 'en_US';
 }
@@ -40,6 +48,7 @@ bind_textdomain_codeset($textDomain, $codeset);
 textdomain($textDomain);
 
 // test translations
+echo "<div>";
 echo _("ayman");
 echo "<br>";
 echo _("xayman");
@@ -51,5 +60,6 @@ echo "<br>";
 echo _("This is me to be translater again and again");
 echo "<br>";
 echo _("this item in your cart already !");
+echo "</div>";
 
-
+include 'bodyend.arabic.template.php';
